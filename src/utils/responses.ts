f22347389e8550c71ed7b0663a9bbfea8ba6be2d
Thamarai <tmanalan710@gmail.com>
2026-02-06 21:07:57 +0530

    added: response structure

---
+++ src/utils/responses.ts
@@ -0,0 +1,17 @@
+const responses = {
+    success: (data: any)=>{
+        return {
+            "success": true,
+            "data": data,
+            "error": null
+        }
+    },
+
+    error: (error: string) => {
+        return {
+            "success": false,
+            "data": null,
+            "error": error
+        }
+    }
+}
