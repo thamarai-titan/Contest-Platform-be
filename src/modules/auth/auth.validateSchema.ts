d1276def1692400009405b21f0c88b9b5a3c6860
Thamarai <tmanalan710@gmail.com>
2026-02-06 22:01:10 +0530

    added: schema for signup

--- src/modules/auth/auth.validateSchema.ts
+++ src/modules/auth/auth.validateSchema.ts
@@ -1,5 +1,22 @@
 import z from "zod"
 
 export const SignupSchema = z.object({
-    
+    name: z.string().min(2, "Give your full name"),
+    email: z.email(),
+    password: z.string().min(6),
+    role: z.enum(["creator", "contestee"], {
+        error: "Role must be a creator or contestee"
+    })
 })
+
+export type SignupSchemaType = z.infer<typeof SignupSchema>
+
+
+export const SignInSchema = z.object({
+    email: z.email(),
+    password: z.string().min(6)
+})
+
+export type SignInSchemaType = z.infer<typeof SignInSchema>
+
+
