9a4f1493933fcdbbdb14d169e7ea78c1c19e3023
Thamarai <tmanalan710@gmail.com>
2026-02-07 21:04:03 +0530

    added: contest route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import authRoute from "./modules/auth/auth.routes.ts"
-
+import contestRoute from "./modules/contests/contests.routes.ts"
 
 const app = express()
 
@@ -9,6 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/api/auth", authRoute)
+app.use("/api", contestRoute)
 
 export default app
 
