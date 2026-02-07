96e8d6a9f9ee723b376713865e0dbc0f75313f29
Thamarai <tmanalan710@gmail.com>
2026-02-07 21:31:24 +0530

    added: service for getcontest

--- src/modules/contests/contests.service.ts
+++ src/modules/contests/contests.service.ts
@@ -25,3 +25,43 @@ export const CreateContestService = async (data: CreateContestsSchemaType, userI
         throw error
     }
 }
+
+
+export const getContestDetailsService = async (contestId: string)=>{
+    try {
+            const contest = await prisma.contests.findUnique({
+                where: {
+                    id: contestId
+                },
+                include: {
+                    mcq_questions: {
+                        select: {
+                            id: true,
+                            question_text: true,
+                            options: true,
+                            points: true
+                        }
+                    },
+                    dsa_problems: {
+                        select: {
+                            id: true,
+                            title: true,
+                            description: true,
+                            tags: true,
+                            points: true,
+                            time_limit: true,
+                            memory_limit: true
+                        }
+                    }
+                }
+            })
+
+            if(!contest){
+                throw new Error("CONTEST_NOT_FOUND")
+            }
+
+            return contest
+    } catch (error) {
+        throw error
+    }
+}
