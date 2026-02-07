14ccdb4ea5eaa144a146d0a11607eb3329e97fb9
Thamarai <tmanalan710@gmail.com>
2026-02-08 00:04:39 +0530

    added: routes for submission

--- src/modules/contests/contests.routes.ts
+++ src/modules/contests/contests.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { requireCreator, verifyUser } from "../../middleware/middleware";
-import { addMcqToContestController, CreateContestController, getContestDetailsController } from "./contests.controller";
+import { requireConteste, requireCreator, verifyUser } from "../../middleware/middleware";
+import { addMcqToContestController, CreateContestController, getContestDetailsController, submitMcqController } from "./contests.controller";
 
 const router = Router()
 
 router.post("/contests", verifyUser, requireCreator, CreateContestController)
 router.get("/contests/:contestId", verifyUser, getContestDetailsController)
 router.post("/contests/:contestId/mcq", verifyUser, requireCreator, addMcqToContestController)
-
+router.post("/contests/:contestId/mcq/:questionId/submit", verifyUser, requireConteste, submitMcqController)
 export default router
