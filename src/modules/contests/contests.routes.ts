5b0f13b207418f0a768183ff60c18e10bbb79416
Thamarai <tmanalan710@gmail.com>
2026-02-07 22:22:30 +0530

    added: route for addMcq

--- src/modules/contests/contests.routes.ts
+++ src/modules/contests/contests.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { requireCreator, verifyUser } from "../../middleware/middleware";
-import { CreateContestController, getContestDetailsController } from "./contests.controller";
+import { addMcqToContestController, CreateContestController, getContestDetailsController } from "./contests.controller";
 
 const router = Router()
 
 router.post("/contests", verifyUser, requireCreator, CreateContestController)
-router.get("/contests/:contestId", verifyUser, requireCreator, getContestDetailsController)
+router.get("/contests/:contestId", verifyUser, getContestDetailsController)
+router.post("/contests/:contestId/mcq", verifyUser, requireCreator, addMcqToContestController)
 
 export default router
