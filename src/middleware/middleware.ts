3188f4e515f7e954e190b43b15b45b7c99496d17
Thamarai <tmanalan710@gmail.com>
2026-02-07 20:59:29 +0530

    added: middleware

---
+++ src/middleware/middleware.ts
@@ -0,0 +1,53 @@
+import type { Request, Response, NextFunction } from "express";
+import { responses } from "../utils/responses";
+import jwt, { decode } from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      role: string;
+    }
+  }
+}
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authToken = req.headers.authorization;
+
+  if (!authToken || !authToken.startsWith("Bearer")) {
+    return res.status(401).json(responses.error("UNAUTHORIZED"));
+  }
+
+  const token = authToken.split(" ")[1] as string;
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as {
+      userId: string;
+      role: string;
+    };
+    req.userId = decoded.userId;
+    req.role = decoded.role;
+    next();
+  } catch (error) {
+    return res.status(401).json(responses.error("UNAUTHORIZED"))
+  }
+};
+
+
+export const requireCreator = (req:Request, res:Response, next:NextFunction) => {
+    if(req.role !== "creator"){
+        return res.status(403).json(responses.error("FORBIDDEN"))
+    }
+    next()
+}
+
+
+export const requireConteste = (req:Request, res:Response, next:NextFunction) => {
+    if(req.role !== "conteste"){ 
+        return res.status(403).json(responses.error("FORBIDDEN"))
+    }
+    next()
+}
