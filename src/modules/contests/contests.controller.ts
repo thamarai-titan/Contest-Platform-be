b815a6cb96b7143fffe74dcf976628490754725f
Thamarai <tmanalan710@gmail.com>
2026-02-07 21:30:16 +0530

    added: getcontests

--- src/modules/contests/contests.controller.ts
+++ src/modules/contests/contests.controller.ts
@@ -1,32 +1,95 @@
-import type {Request, Response} from "express"
-import { CreateContestsSchema, type CreateContestsSchemaType } from "./contests.validateSchema"
-import { CreateContestService } from "./contests.service"
-import { responses } from "../../utils/responses"
+import type { Request, Response } from "express";
+import {
+  CreateContestsSchema,
+  type CreateContestsSchemaType,
+} from "./contests.validateSchema";
+import {
+  CreateContestService,
+  getContestDetailsService,
+} from "./contests.service";
+import { responses } from "../../utils/responses";
 
-export const CreateContestController = async (req: Request, res: Response)=>{
-    try {
-        const data: CreateContestsSchemaType = CreateContestsSchema.parse(req.body)
-        const userId = req.userId
+type Params = {
+  contestId: string;
+};
 
-        const contest = await CreateContestService(data,userId)
+export const CreateContestController = async (req: Request, res: Response) => {
+  try {
+    const data: CreateContestsSchemaType = CreateContestsSchema.parse(req.body);
+    const userId = req.userId;
 
-        if(!contest){
-            return res.status(400).json(responses.error("NOT_CREATED"))
-        }
+    const contest = await CreateContestService(data, userId);
 
-        res.status(201).json(responses.success({
-            id: contest.id,
-            title: contest.title,
-            description: contest.description,
-            creatorId: contest.creator_id,
-            startTime: contest.start_time,
-            endTime: contest.end_time
-        }))
-    } catch (error: any) {
-        if(error.name === "ZodError"){
-            res.status(400).json(responses.error("INVALID_REQUEST"))
-        }
+    if (!contest) {
+      return res.status(400).json(responses.error("NOT_CREATED"));
+    }
 
-        res.status(400).json(responses.error("INTERNAL_SERVER_ERROR"))
-    }   
-}
+    res.status(201).json(
+      responses.success({
+        id: contest.id,
+        title: contest.title,
+        description: contest.description,
+        creatorId: contest.creator_id,
+        startTime: contest.start_time,
+        endTime: contest.end_time,
+      }),
+    );
+  } catch (error: any) {
+    if (error.name === "ZodError") {
+      res.status(400).json(responses.error("INVALID_REQUEST"));
+    }
+
+    res.status(400).json(responses.error("INTERNAL_SERVER_ERROR"));
+  }
+};
+
+export const getContestDetailsController = async (
+  req: Request<Params>,
+  res: Response,
+) => {
+  try {
+    const { contestId } = req.params;
+
+    const contest = await getContestDetailsService(contestId);
+
+    if (!contest) {
+      res.status(404).json(responses.error("CONTEST_NOT_FOUND"));
+    }
+
+    const mcqsmap = contest.mcq_questions.map((data) => ({
+      id: data.id,
+      questionText: data.question_text,
+      options: data.options,
+      points: data.points,
+    }));
+
+    const dsamap = contest.dsa_problems.map((data) => ({
+      id: data.id,
+      title: data.title,
+      description: data.description,
+      tags: data.tags,
+      points: data.points,
+      timeLimit: data.time_limit,
+      memoryLimit: data.memory_limit,
+    }));
+
+    res.status(200).json(
+      responses.success({
+        id: contest.id,
+        title: contest.title,
+        description: contest.description,
+        startTime: contest.start_time,
+        endTime: contest.end_time,
+        creatorId: contest.creator_id,
+        mcqs: mcqsmap,
+        dsaProblems: dsamap,
+      }),
+    );
+  } catch (error: any) {
+    if (error.message === "CONTEST_NOT_FOUND") {
+      res.status(404).json(responses.error("CONTEST_NOT_FOUND"));
+    }
+
+    res.status(400).json(responses.error("INTERNAL_SERVER_ERROR"));
+  }
+};
