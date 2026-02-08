fb15818844e9469059f8d47eaebeab34c564402e
Thamarai <tmanalan710@gmail.com>
2026-02-08 21:35:16 +0530

    schema for dsa problems

--- src/modules/contests/contests.validateSchema.ts
+++ src/modules/contests/contests.validateSchema.ts
@@ -20,3 +20,21 @@ export const McqCreationSchema = z.object({
 
 export type McqCreationsSchemaType = z.infer<typeof McqCreationSchema>
 
+const testCaseArraySchema = z.object({
+        input: z.string(),
+        expectedOutput: z.string(),
+        isHidden: z.boolean()
+    })
+
+export const DsaCreationSchema = z.object({
+    title: z.string().min(3, "must give a title"),
+    description: z.string().min(5, "must give the description about the problem"),
+    tags: z.string().array(),
+    points: z.number().min(1),
+    timeLimit: z.number().min(1),
+    memoryLimit: z.number().min(1),
+    testCases: z.array(testCaseArraySchema)
+})
+
+export type DsaCreationSchemaType = z.infer<typeof DsaCreationSchema>
+export type testCaseArraySchemaType = z.infer<typeof testCaseArraySchema>
