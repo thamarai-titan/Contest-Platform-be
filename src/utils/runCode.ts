eb4098605e6477b3427ec87e573c98d2c03806c6
Thamarai <tmanalan710@gmail.com>
2026-02-10 21:22:36 +0530

    modified run code

--- src/utils/runCode.ts
+++ src/utils/runCode.ts
@@ -1,4 +1,4 @@
-import { exec } from "child_process";
+import { execFile } from "child_process";
 import fs from "fs";
 import path from "path";
 
@@ -8,44 +8,60 @@ export function runCode(
 ): Promise<any> {
   return new Promise((resolve, reject) => {
     const id = Date.now().toString();
-    const runDir = path.join(process.cwd(), "..", "tmp", id);
+    const runDir = path.join(process.cwd(), "tmp", id);
 
-    fs.mkdirSync(runDir, { recursive: true });
+    try {
+      fs.mkdirSync(runDir, { recursive: true });
 
-    // 1️⃣ write user code
-    fs.writeFileSync(
-      path.join(runDir, "solution.js"),
-      `
-      module.exports = function solve(input) {
-        ${code}
-      }
-      `
-    );
 
-    // 2️⃣ write testcases
-    fs.writeFileSync(
-      path.join(runDir, "testcases.json"),
-      JSON.stringify(testcases)
-    );
+      fs.writeFileSync(
+        path.join(runDir, "solution.js"),
+        `
+module.exports = function solve(input) {
+${code}
+}
+`
+      );
 
-    // 3️⃣ run docker
-    const command = `
-      docker run --rm \
-        --memory=128m \
-        --cpus=0.5 \
-        --network none \
-        -v ${runDir}:/app \
-        js-code-runner
-    `;
 
-    exec(command, { timeout: 3000 }, (err, stdout, stderr) => {
+      fs.writeFileSync(
+        path.join(runDir, "testcases.json"),
+        JSON.stringify(testcases)
+      );
+
+
+      execFile(
+        "docker",
+        [
+          "run",
+          "--rm",
+          "--memory=128m",
+          "--cpus=0.5",
+          "--network",
+          "none",
+          "-v",
+          `${runDir}:/app`,
+          "js-code-runner"
+        ],
+        { timeout: 3000 },
+        (err, stdout, stderr) => {
+          fs.rmSync(runDir, { recursive: true, force: true });
+
+          if (err) {
+            return reject(stderr || err.message);
+          }
+
+          try {
+            const result = JSON.parse(stdout);
+            resolve(result);
+          } catch (parseError) {
+            reject("Invalid runner output");
+          }
+        }
+      );
+    } catch (error: any) {
       fs.rmSync(runDir, { recursive: true, force: true });
-
-      if (err) {
-        return reject(stderr || err.message);
-      }
-
-      resolve(JSON.parse(stdout));
-    });
-  });
+      reject(error.message);
+    }
+  });                                                                                                        
 }
